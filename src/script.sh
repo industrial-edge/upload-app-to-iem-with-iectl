1012fa2da1e203f62d95c3add4368f4929436ea4
Pavel Halama <pavel.halama@siemens.com>
2021-05-03 13:36:57 +0200

    initial commit

--- src/script.sh
+++ src/script.sh
@@ -11,22 +11,21 @@ ie-app-publisher-linux ws i
 echo "-----------------------------------------CONNECT TO DOCKER ENGINE-----------------------------------------"
 ie-app-publisher-linux de c -u http://localhost:2375
 
-# Portal login and logout 
+# Portal login
 echo "-----------------------------------------IEM LOGIN--------------------------------------------------------"
 export IE_SKIP_CERTIFICATE=true # DO THIS IN TRUSTED ENVIROMENT ONLY! 
-ie-app-publisher-linux em li -u <iem-url> -e <iem-username> -p <iem-password>
+ie-app-publisher-linux em li -u $IEM_URL -e $IEM_USERNAME -p $IEM_PASSWORD
 
-# Create applications version 
 echo "-----------------------------------------UPLOAD APP TO IEM------------------------------------------------"
 
-# application version management
-version=$(ie-app-publisher-linux em app dt -a <app-ID> -p | \
+# Application version management
+version=$(ie-app-publisher-linux em app dt -a $APP_ID -p | \
     python3 -c "import sys, json; print(json.load(sys.stdin)['versions'][0]['versionNumber'])")
 echo 'old Version: '$version
 version_new=$(echo $version | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}')
 echo 'new Version: '$version_new
 
-
-ie-app-publisher-linux em app cuv -a <app-ID> -v $version_new -y <path-to-docker-compose.yml> -n '{"hello-edge":[{"name":"hello-edge","protocol":"HTTP","port":"80","headers":"","rewriteTarget":"/"}]}' -s 'hello-edge' -t 'FromBoxReverseProxy' -u "hello-edge" -r "/"
-ie-app-publisher-linux em app uuv -a <app-ID> -v $version_new
+# Create and upload application version to IEM 
+ie-app-publisher-linux em app cuv -a $APP_ID -v $version_new -y $DOCKER_COMP_PATH -n '{"hello-edge":[{"name":"hello-edge","protocol":"HTTP","port":"80","headers":"","rewriteTarget":"/"}]}' -s 'hello-edge' -t 'FromBoxReverseProxy' -u "hello-edge" -r "/"
+ie-app-publisher-linux em app uuv -a $APP_ID -v $version_new
 
