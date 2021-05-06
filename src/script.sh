2181788e570ddf671ad355b23d47e7e36e7f5fd8
Pavel Halama <pavel.halama@siemens.com>
2021-05-06 10:38:36 +0200

    license.md

--- src/script.sh
+++ src/script.sh
@@ -1,3 +1,7 @@
+# Copyright 2020 Siemens AG
+# This file is subject to the terms and conditions of the MIT License.  
+# See LICENSE file in the top-level directory.
+
 # Set envriroment variables 
 export IE_URL="https://<ip>:9443"
 export IE_USER="<iem-username>"
